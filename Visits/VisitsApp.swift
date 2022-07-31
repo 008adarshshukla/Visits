6ae32ba1f384a4d315ac68d5462d3de58cdcef8b
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-07-31 09:47:17 +0530

    Initial Commit

---
+++ Visits/VisitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitsApp.swift
+//  Visits
+//
+//  Created by Adarsh Shukla on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct VisitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
