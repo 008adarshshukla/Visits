be843a615a0e9a976040d091d1c9408f6c2db6ba
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-11-01 20:59:52 +0530

    added side bar location view

--- Visits/Views/LocationsView.swift
+++ Visits/Views/LocationsView.swift
@@ -12,14 +12,31 @@ struct LocationsView: View {
     
     @StateObject private var viewModel = LocationsViewModel()
     @State var searchedLocation: String = ""
-    
+    @State var currenTab: String = "home"
+    @Namespace var animation
+    @State var showSideBar: Bool = false
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
-                .ignoresSafeArea()
+            ZStack {
+                Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                    .ignoresSafeArea()
+                
+                searchBar()
+            }
+            .onTapGesture {
+                withAnimation(Animation.easeInOut) {
+                    showSideBar = false
+                }
+            }
             
-            searchBar()
+            ScrollView(showsIndicators: false) {
+                SideBar()
+            }
+            .background(content: {
+                Color.white.ignoresSafeArea()
+            })
+            .offset(x: showSideBar ? -165 : -300)//-165
         }
         
     }
@@ -32,12 +49,17 @@ struct LocationsView_Previews: PreviewProvider {
 }
 
 extension LocationsView {
+    
+    //MARK: Search Bar
     @ViewBuilder
     func searchBar() -> some View {
         VStack {
             HStack {
                 Button {
                     //present side bar.
+                    withAnimation(Animation.easeInOut) {
+                        showSideBar = true
+                    }
                 } label: {
                     Image(systemName: "line.3.horizontal")
                         .font(.title2)
@@ -65,6 +87,88 @@ extension LocationsView {
             .padding()
             
             Spacer()
+        }
+    }
+    
+    //MARK: Side Bar
+    @ViewBuilder
+    func SideBar() -> some View {
+        
+        //MARK: Tabs
+        let tabs: [String] = [
+            "home",
+            "bakery",
+            "cafe",
+            "carRental",
+            "fitnessCenter",
+            "hospital",
+            "hotel",
+            "laundry",
+            "movieTheater",
+            "pharmacy",
+            "restaurant",
+            "store",
+            "university"
+        ]
+        
+        VStack(spacing: 10) {
+            Image("LogoImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 55, height: 55)
+                .padding(.bottom, 10)
+            
+            ForEach(tabs, id: \.self) { tab in
+                VStack(spacing: 8) {
+                    Image("\(tab)")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 22)
+                    Text(tab)
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(currenTab == tab ? Color.orange : Color.gray)
+                .padding(.vertical, 13)
+                .frame(width: 100)
+                .background {
+                    if currenTab == tab {
+                        RoundedRectangle(cornerRadius: 15, style: .continuous)
+                            .fill(Color.orange.opacity(0.3))
+                            .matchedGeometryEffect(id: "TAB", in: animation)
+                    }
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        currenTab = tab
+                    }
+                }
+            }
+            
+            Button {
+                
+            } label: {
+                VStack {
+                    Image("user")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 45, height: 45)
+                        .clipShape(Circle())
+                    Text("Profile")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.top, 20)
+        }
+        .padding(.vertical)
+        .frame(maxHeight: .infinity, alignment: .top)
+        .frame(width: 100)
+        .background {
+            Color.white
+                .ignoresSafeArea()
         }
     }
 }
